91b5445e4e6569d846dac30e12727e69f8b2ecd6
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-06-01 15:30:50 -0300

    custom hook useWordGame created

--- src/App.js
+++ src/App.js
@@ -1,63 +1,18 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
+import useWordGame from "./hooks/useWordGame";
 
 function App() {
-  const STARTING_TIME = 10;
-
-  const [typedWords, setTypedWords] = useState("");
-  const [timer, setTimer] = useState(STARTING_TIME);
-  const [toggle, setToggle] = useState(false);
-  const [wordsCount, setWordsCount] = useState(0);
-  const [highScore, setHighScore] = useState(0);
-
-  const inputRef = useRef(null);
-
-  const handleChange = (event) => {
-    setTypedWords(event.target.value);
-  };
-
-  const handleCountWords = (text) => {
-    const wordsArr = text.split(" ");
-    return wordsArr.filter((word) => word !== "").length;
-  };
-
-  const blockPaste = (event) => {
-    event.target.addEventListener("paste", (e) => {
-      e.preventDefault();
-      return false;
-    });
-  };
-
-  const calculateHighScore = () => {
-    if (wordsCount > highScore) {
-      setHighScore(wordsCount);
-    }
-  };
-
-  const startGame = () => {
-    setToggle(true);
-    setTimer(STARTING_TIME);
-    setTypedWords("");
-    inputRef.current.disabled = false;
-    inputRef.current.focus();
-  };
-
-  const endGame = () => {
-    setWordsCount(handleCountWords(typedWords));
-    setToggle(false);
-    calculateHighScore();
-  };
-
-  useEffect(() => {
-    if (timer > 0 && toggle) {
-      setWordsCount(0);
-      setTimeout(() => {
-        setTimer((prevTimer) => prevTimer - 1);
-      }, 1000);
-    } else if (timer === 0) {
-      endGame();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [timer, toggle]);
+  const {
+    inputRef,
+    toggle,
+    handleChange,
+    typedWords,
+    blockPaste,
+    timer,
+    startGame,
+    highScore,
+    wordsCount,
+  } = useWordGame();
 
   return (
     <>
